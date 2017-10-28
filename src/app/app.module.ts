5430e6cffc86d2ad42863b04c96e639fbe72c719
jatin7gupta <jatin13csu055@ncuindia.edu>
2017-10-28 13:03:38 +0530

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopLeftComponent } from './top-left/top-left.component';
+import { CenterComponent } from './center/center.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopLeftComponent,
+    CenterComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
